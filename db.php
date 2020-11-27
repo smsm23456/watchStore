42b042eb6837cf016c61f64a320ebdfa92cb5d15
Latifa-sar <Latifa.a.sar@gmail.com>
2020-11-27 18:35:57 +0300

    update db

--- db.php
+++ db.php
@@ -22,19 +22,6 @@ session_start();
        	if ($password_1 != $password_2) {
 			 array_push($errors, "Password not match");}
 
-       $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
- $result = mysqli_query($db, $user_check_query);
- $user = mysqli_fetch_assoc($result);
-
- if ($user) { // if user exists
-   if ($user['username'] === $username) {
-     array_push($errors, "Username already exists");
-   }
-
-   if ($user['email'] === $email) {
-     array_push($errors, "email already exists");
-   }
- }
          if (count($errors) == 0){
         $password=md5($password_1);
         $query="insert into user(name,username,email,password)Values('$name','$username','$email','$password')";
